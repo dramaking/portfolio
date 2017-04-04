376296173ba85b51c424cdf320445626760eb7d2
dramaking <wzc1986@gmail.com>
2017-04-04 17:10:05 +1200

    deleted redundant code

--- gulpfile.js
+++ gulpfile.js
@@ -14,51 +14,15 @@ var cache = require('gulp-cache');
 var del = require('del');
 var runSequence = require('run-sequence');
 
-// Set the banner content
-var banner = ['/*!\n',
-    ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
-    ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
-    ' * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n',
-    ' */\n',
-    ''
-].join('');
-
-// Compiles SCSS files from /scss into /css
-// NOTE: This theme uses LESS by default. To swtich to SCSS you will need to update this gulpfile by changing the 'less' tasks to run 'sass'!
 gulp.task('sass', function() {
     return gulp.src('app/scss/agency.scss')
         .pipe(sass())
-        .pipe(header(banner, { pkg: pkg }))
         .pipe(gulp.dest('app/css'))
         .pipe(browserSync.reload({
             stream: true
         }))
 });
 
-// Minify compiled CSS
-// gulp.task('minify-css', ['sass'], function() {
-//     return gulp.src('css/agency.css')
-//         .pipe(cleanCSS({ compatibility: 'ie8' }))
-//         .pipe(rename({ suffix: '.min' }))
-//         .pipe(gulp.dest('css'))
-//         .pipe(browserSync.reload({
-//             stream: true
-//         }))
-// });
-
-// Minify JS
-// gulp.task('minify-js', function() {
-//     return gulp.src('js/agency.js')
-//         .pipe(uglify())
-//         .pipe(header(banner, { pkg: pkg }))
-//         .pipe(rename({ suffix: '.min' }))
-//         .pipe(gulp.dest('js'))
-//         .pipe(browserSync.reload({
-//             stream: true
-//         }))
-// });
-
-// Copy vendor libraries from /node_modules into /vendor
 gulp.task('copy', function() {
     gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
         .pipe(gulp.dest('app/vendor/bootstrap'))
@@ -113,11 +77,8 @@ gulp.task('browserSync', function() {
 })
 
 // Dev task with browserSync
-gulp.task('watch', ['browserSync', 'sass', 'minify-css', 'minify-js'], function() {
+gulp.task('watch', ['browserSync', 'sass'], function() {
     gulp.watch('app/scss/*.scss', ['sass']);
-    //gulp.watch('css/*.css', ['minify-css']);
-    //gulp.watch('js/*.js', ['minify-js']);
-    // Reloads the browser whenever HTML or JS files change
     gulp.watch('app/*.html', browserSync.reload);
     gulp.watch('app/js/**/*.js', browserSync.reload);
 });
